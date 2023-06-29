1979cda1c735b6706d8c01cfa8e59a0d9b7f0491
kip <kkipngenokoech22@gmail.com>
2023-06-29 10:23:18 +0300

    task updater

--- tutorverse/src/components/taskUpdater.js
+++ tutorverse/src/components/taskUpdater.js
@@ -1,9 +1,110 @@
-function TaskUpdater(){
-    return (
-        <div>
-            <h1>Hello Multiverse</h1>
-        </div>
-    )
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import AddTaskIcon from '@mui/icons-material/AddTask';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Grid } from '@mui/material';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        Multiverse
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default TaskUpdater
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+const tasks = []
+export default function TaskUpdater() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const task =  data.get('task')
+    const taskimportance = data.get('task-importance')
+    tasks.push(task)
+    console.log(tasks)
+    console.log({
+        task: task,
+        importance: taskimportance
+        
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <AddTaskIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Task Updater
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="task"
+              label="Task "
+              name="task"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              name="task-importance"
+              label="task importance"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Add Task 
+            </Button>
+            {tasks.length}
+          </Box>
+        </Box>
+        <Grid>
+        {tasks.map((task) => {
+            return <li>{task}</li>
+        })}
+        </Grid>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
