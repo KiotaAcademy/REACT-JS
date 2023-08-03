f959b24518dcd9b1ab0efb10d7b01d78d1d16566
kip <kkipngenokoech22@gmail.com>
2023-08-03 22:11:57 +0300

    react router

--- multiversek/src/App.jsx
+++ multiversek/src/App.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 import Header from './components/header'
 
 function App() {
+  const [allHouses, setAllHouses] = useState([])
+  useEffect(()=>{
+    const fetchHouses = async () => {
+      const response = await fetch('./houses.json')
+      const houses = await response.json()
+      setAllHouses(houses)
+    }
+    fetchHouses()
+  },[])
 
+  const featuredHouse = useMemo(()=>{
+    if (allHouses.length){
+      const randomIndex = Math.floor(Math.random() * allHouses.length)
+      return allHouses[randomIndex]
+    }
+  }, [allHouses])
   return (
     <>
     <div className='container'>
