e045696dd8657677a70c14b814d935232c256bfa
kip <kkipngenokoech22@gmail.com>
2023-08-03 18:26:34 +0300

    bootstrap is working

--- multiversek/src/components/header.jsx
+++ multiversek/src/components/header.jsx
@@ -2,10 +2,10 @@ export default function Header(){
     return (
         <>
         <header className = "row">
-            <div>
-                <img src="/images/klogo.jpg" alt="Logo" />
+            <div className="col-md-2">
+                <img src="/images/klogo.jpg" alt="Logo" className="img-fluid logo"/>
             </div>
-            <div className='col-md-7 mt-5 subtitle' alt = "logo">
+            <div className='col-md-7 mt-5 subtitle ' alt = "logo">
                 providing houses all over the world
             </div>
         </header>
